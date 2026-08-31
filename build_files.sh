956fb804aa10fdc7564ef3ab7de365722d70e0bc
siwadol rukchart <hikiyasabuno@gmail.com>
2026-09-01 02:13:01 +0700

    Add --break-system-packages flag for PEP 668 compatibility on Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
 mkdir -p staticfiles_build
+touch staticfiles_build/placeholder.txt
+
 
