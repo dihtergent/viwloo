34436584be1ffacfde7808a79702024dfb3a804b
siwadol rukchart <hikiyasabuno@gmail.com>
2026-09-01 01:55:23 +0700

    fix1 static path

--- viewvoo/settings.py
+++ viewvoo/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if os.path.isdir(os.path.join(BASE_DIR, 'static')) else []
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
