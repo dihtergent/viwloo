160d941faeb18f6225a94ae1c4aaa4d8757e9d2a
siwadol rukchart <hikiyasabuno@gmail.com>
2026-09-01 02:17:11 +0700

    Fix STATIC_URL leading slash and STATIC_ROOT subfolder path for Vercel CDN

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
-mkdir -p staticfiles_build
-touch staticfiles_build/placeholder.txt
+mkdir -p staticfiles_build/static
+touch staticfiles_build/static/placeholder.txt
+
 
 
--- viewvoo/settings.py
+++ viewvoo/settings.py
@@ -137,9 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] if os.path.isdir(os.path.join(BASE_DIR, 'static')) else []
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
