835aac9be217d589f7e44d1b60084bb20b350f09
badsigret-alt <badsigret@gmail.com>
2026-09-04 14:00:37 +0700

    addd thing sone thing i think maybe idk i dont care i gonna break ts

--- web/models.py
+++ web/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-# Create your models here.
 
 class images(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='posts')
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default='')
     image = CloudinaryField('image')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title or f"Image #{self.id}"
